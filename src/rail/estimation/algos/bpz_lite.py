6dadb1925694351b94cf1ca176043262210878eb
taiwithers <59430904+taiwithers@users.noreply.github.com>
2025-11-27 13:33:08 -0500

    Add `entrypoint_function` properties

--- src/rail/estimation/algos/bpz_lite.py
+++ src/rail/estimation/algos/bpz_lite.py
@@ -65,6 +65,7 @@ class BPZliteInformer(CatInformer):
     """
 
     name = "BPZliteInformer"
+    entrypoint_function = "inform"  # the user-facing science function for this class
     config_options = CatInformer.config_options.copy()
     config_options.update(
         zmin=SHARED_PARAMS,
@@ -308,6 +309,7 @@ class BPZliteEstimator(CatEstimator):
     """
 
     name = "BPZliteEstimator"
+    entrypoint_function = "estimate"  # the user-facing science function for this class
     config_options = CatEstimator.config_options.copy()
     config_options.update(
         zmin=SHARED_PARAMS,
